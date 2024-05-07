173bdb48eeed019c11c80fc05351f1efe4038ecd
tetsuji <tetsu0332df@gmail.com>
2024-05-07 21:04:46 +0900

    add アプリを追加

---
+++ PythonSampleApp/PythonSampleApp/AppDelgate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelgate.swift
+//  PythonSampleApp
+//
+//  Created by 松島哲仁 on 2024/05/07.
+//
+
+import Foundation
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        
+    }
+}
